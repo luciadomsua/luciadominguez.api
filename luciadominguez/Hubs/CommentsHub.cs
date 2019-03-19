6999a60d3c4e9a869f6a433320d52d9e04142163
Lucía Domínguez <luciadomsua@gmail.com>
2019-03-19 18:42:36 +0100

    Añadido Hub SignalR para comentarios en tiempo real.

---
+++ luciadominguez/Hubs/CommentsHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+using System;
+using System.Threading.Tasks;
+
+namespace luciadominguez.Hubs
+{
+    public class CommentsHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+    }
+}
--- luciadominguez/Startup.cs
+++ luciadominguez/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using luciadominguez.Hubs;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace luciadominguez
 {
@@ -25,6 +20,8 @@ namespace luciadominguez
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,6 +37,10 @@ namespace luciadominguez
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSignalR(configure => {
+                configure.MapHub<CommentsHub>("hubs/comments");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
